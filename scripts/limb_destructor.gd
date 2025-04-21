9715ab56d0257223fc60471eafe063f12777a937
Uhboer <grishukoleg258@gmail.com>
2025-04-21 21:13:23 +0300

    spritos + states for limbeater

--- scripts/limb_destructor.gd
+++ scripts/limb_destructor.gd
@@ -6,6 +6,8 @@ extends Node2D
 
 @onready var debug_text = $debug_text
 
+@onready var bloody = $icon/bloody
+
 func _physics_process(delta):
 	debug_text.text = "destruct: "+limb
 
@@ -13,9 +15,13 @@ func _on_body_entered(body: Node2D):
 	if body.name == "character":
 		if limb == "lleg":
 			Global.llegHP -= 100
+			bloody.visible = true
 		if limb == "rleg":
 			Global.rlegHP -= 100
+			bloody.visible = true
 		if limb == "rarm":
 			Global.rarmHP -= 100
+			bloody.visible = true
 		if limb == "larm":
 			Global.larmHP -= 100
+			bloody.visible = true
