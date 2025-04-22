8205f436a6a8137d5c4e138ad143a8bc9d86042b
Uhboer <grishukoleg258@gmail.com>
2025-04-22 14:10:51 +0300

    character list

--- scripts/global.gd
+++ scripts/global.gd
@@ -7,3 +7,6 @@ var rlegHP = 100
 var llegHP = 100
 var rarmHP = 100
 var larmHP = 100
+
+var playername = "Nameless"
+var age = 18
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,13 +1,45 @@
 extends Control
-@onready var button = $Button
+@onready var play = $play
 @onready var click = $sounds/click
 @onready var hover = $sounds/hover
+@onready var make = $make
+@onready var age = $make/age
+@onready var readyfordeath = $make/readyfordeath
+@onready var playername = $make/name
+@onready var accept = $sounds/accept
 
 
 func _on_button_pressed():
 	click.play()
-	get_tree().change_scene_to_file("res://csenes/world.tscn")
+	play.visible = false
+	make.visible = true
 
 
 func _on_button_mouse_entered():
 	hover.play()
+
+
+func _on_readyfordeath_pressed():
+	click.play()
+	get_tree().change_scene_to_file("res://csenes/world.tscn")
+
+
+func _on_readyfordeath_mouse_entered():
+	hover.play()
+
+
+func _on_name_text_submitted(new_text):
+	Global.playername = playername.text
+	accept.play()
+
+func _on_name_text_changed(new_text):
+	Global.playername = playername.text 
+
+
+func _on_age_text_changed(new_text):
+	Global.age = age.text
+
+
+func _on_age_text_submitted(new_text):
+	Global.age = age.text
+	accept.play()
