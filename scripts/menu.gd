5e61c1bb4462f4e3b215bb8e2be5191326492ccc
Uhboer <grishukoleg258@gmail.com>
2025-04-22 13:19:04 +0300

    better way

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,12 +1,12 @@
-extends Node2D
+extends Control
 @onready var button = $Button
 @onready var click = $sounds/click
 @onready var hover = $sounds/hover
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://csenes/world.tscn")
 	click.play()
+	get_tree().change_scene_to_file("res://csenes/world.tscn")
 
 
 func _on_button_mouse_entered():
