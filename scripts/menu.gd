5ae1f9ec546c292255d48f8afdd7a27b57ca26af
Uhboer <grishukoleg258@gmail.com>
2025-04-22 13:08:42 +0300

    update menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,6 +1,13 @@
 extends Node2D
 @onready var button = $Button
+@onready var click = $sounds/click
+@onready var hover = $sounds/hover
 
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://csenes/world.tscn")
+	click.play()
+
+
+func _on_button_mouse_entered():
+	hover.play()
