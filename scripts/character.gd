f4b7f22bc1d5d9200e81ec15cb03ed4076a2cc19
NeverMind92 <llegionov2@gmail.com>
2025-04-27 21:50:06 +0400

    Base CombatMode

--- scripts/character.gd
+++ scripts/character.gd
@@ -5,6 +5,10 @@ extends CharacterBody2D
 
 @onready var body = $layouts/body
 
+@onready var combatModeSound = $sounds/combatmode
+@onready var combatModeOffSound = $sounds/combatmodeoff
+
+var isEnable = false 
 
 func _physics_process(delta):
 	var direction = Input.get_vector("A", "D", "W", "S")
@@ -17,3 +21,12 @@ func _physics_process(delta):
 		animLleg.play("Idle")
 		animRleg.play("Idle")
 	move_and_slide()
+	combatmode() 
+
+func combatmode():
+	if Input.is_action_just_released("F"):
+		isEnable = !isEnable  # Инвертируем значение
+		if isEnable:
+			combatModeSound.play()
+		else:
+			combatModeOffSound.play()
